55cdfccc67db345faa100aac2cfc98f9cbef026a
Svyatoslav <manakhovsvyatoslav@gmail.com>
2020-11-23 16:12:39 +0300

    added time_adding

--- task_6.py
+++ task_6.py
@@ -1,10 +1,20 @@
 from glob import glob
 from PIL import Image
+import time
+
+
+def rename_file(filename_old):
+    dot_number = filename_old.rindex(".")
+    extension = filename_old[dot_number:]
+    time_now = time.strftime("_%d.%m.%Y.%H.%M", time.localtime())
+    filename_new = filename_old.replace("new_photo", "rendered_photo")
+    filename_new = filename_new.replace(extension, time_now + extension)
+    return filename_new
+
 
 list_file = glob("new_photo/*")
-print(list_file)
 for file in list_file:
     with Image.open(file) as picture:
         picture_rendered = picture.convert("L")
-    file_rendered = file.replace("new_photo", "rendered_photo")
+    file_rendered = rename_file(file)
     picture_rendered.save(file_rendered)
